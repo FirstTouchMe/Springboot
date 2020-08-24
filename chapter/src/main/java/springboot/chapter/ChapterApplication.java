dfd680989f37ee6360740b9c624a867dc9d44836
FirstTouchMe <1206278229@qq.com>
2020-08-24 14:45:30 +0800

    第一章节

---
+++ chapter/src/main/java/springboot/chapter/ChapterApplication.java
@@ -0,0 +1,13 @@
+package springboot.chapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//springboot的入门文件
+@SpringBootApplication
+public class ChapterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChapterApplication.class, args);
+    }
+
+}
---
+++ chapter/src/main/webapp/WEB/jsp/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 12062
+  Date: 2020/8/24
+  Time: 10:34
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>index测试</title>
+</head>
+<body>
+<h1>Hello,World</h1>
+
+</body>
+</html>
